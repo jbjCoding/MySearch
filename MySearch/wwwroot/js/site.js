a1bca57cbd22f25aa62a9116e4fd5730bd6b297c
Josip Bodružić <jbj.coding@gmail.com>
2022-07-07 14:42:38 +0200

    add search

--- MySearch/wwwroot/js/site.js
+++ MySearch/wwwroot/js/site.js
@@ -1,22 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const searchResults = [];
 
-// Write your JavaScript code.
-
-$(document).ready(function () {
-    console.log('ready');
-});
+const urls = [];
 
 const baseUrl = 'https://www.googleapis.com/customsearch/v1?'
 const apiKey = 'AIzaSyCPlZG7tzO8CEq81u_ty0eu8mHs8nbo9BI';
 const cx = '30ea3021d27d247fe';
 
-const url = baseUrl + 'key=' + apiKey + '&cx=' + cx + '&q=';
+const createUrl = function (start, query) {
+    const url = baseUrl + 'key=' + apiKey + '&cx=' + cx + `&start=${start}` + `&q=${query}`;
+    return url;
+}
+
 
 $('#search-button').on('click', function () {
-    $.get({
-        url: url + 'lectures'
-    }).done(function(res) {
-        console.log(res);
-    });
+    const searchVal = $('#search-input').val();
+
+    if (!inputValid(searchVal)) {
+        alert('search query can\'t be empty');
+        return;
+    }
+
+    for (var i = 0; i < 10; i++) {
+        const url = createUrl(1 + i * 10, searchVal);
+        urls.push(url);
+    }
+
+    for (var i = 0; i < 10; i++) {
+        $.get({
+            url: urls[i]
+        }).done(function (res) {
+            res.items.forEach(i => searchResults.push(i));
+        });
+    }
 });
+
+const inputValid = function (val) {
+    return val.length !== 0;
+}
