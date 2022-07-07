adf9208ab1df4e039fc88da35b9e0e435e6b856a
Josip Bodružić <jbj.coding@gmail.com>
2022-07-07 13:08:20 +0200

    initial

---
+++ MySearch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MySearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
