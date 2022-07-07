5394efb037aae2b0542fb15a3756e806262f1ed1
Josip Bodružić <jbj.coding@gmail.com>
2022-07-07 13:51:20 +0200

    setup google search api

---
+++ MySearch/DAL/MySearchContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MySearch.DAL
+{
+    public class MySearchContext : DbContext
+    {
+        public MySearchContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- MySearch/wwwroot/js/site.js
+++ MySearch/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    console.log('ready');
+});
+
+const baseUrl = 'https://www.googleapis.com/customsearch/v1?'
+const apiKey = 'AIzaSyCPlZG7tzO8CEq81u_ty0eu8mHs8nbo9BI';
+const cx = '30ea3021d27d247fe';
+
+const url = baseUrl + 'key=' + apiKey + '&cx=' + cx + '&q=';
+
+$('#search-button').on('click', function () {
+    $.get({
+        url: url + 'lectures'
+    }).done(function(res) {
+        console.log(res);
+    });
+});
